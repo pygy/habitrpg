05655b7cdbbf975831ee361941ca0956b507d955
Pierre-Yves Gerardy <py@py.gy>
2014-04-19 21:58:00 +0200

    remove tracking

--- public/js/controllers/footerCtrl.js
+++ public/js/controllers/footerCtrl.js
@@ -12,22 +12,22 @@
     $scope.deferredScripts = function(){
 
       // Stripe
-      $.getScript('//checkout.stripe.com/v2/checkout.js');
+      // $.getScript('//checkout.stripe.com/v2/checkout.js');
 
       // Google Analytics, only in production
-      if (window.env.NODE_ENV === 'production') {
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-        ga('create', window.env.GA_ID, 'habitrpg.com');
-        ga('send', 'pageview');
-      }
+      // if (window.env.NODE_ENV === 'production') {
+      //   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      //     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      //   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      //   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      //   ga('create', window.env.GA_ID, 'habitrpg.com');
+      //   ga('send', 'pageview');
+      // }
 
       // Scripts only for desktop
       if (!window.env.IS_MOBILE) {
         // Add This
-        $.getScript("//s7.addthis.com/js/250/addthis_widget.js#pubid=lefnire");
+        // $.getScript("//s7.addthis.com/js/250/addthis_widget.js#pubid=lefnire");
 
         // Google Charts
         $.getScript("//www.google.com/jsapi", function() {
@@ -42,30 +42,30 @@
     /**
      * Debug functions. Note that the server route for gems is only available if process.env.DEBUG=true
      */
-    if (window.env.NODE_ENV === 'development') {
-      $scope.addMissedDay = function(){
-        if (!confirm("Are you sure you want to reset the day?")) return;
-        var dayBefore = moment(User.user.lastCron).subtract('days', 1).toDate();
-        User.set({'lastCron': dayBefore});
-        Notification.text('-1 day, remember to refresh');
-      }
-      $scope.addTenGems = function(){
-        $http.post(API_URL + '/api/v2/user/addTenGems').success(function(){
-          User.log({});
-        })
-      }
-      $scope.addLevelsAndGold = function(){
-        User.set({
-          'stats.exp': User.user.stats.exp + 10000,
-          'stats.gp': User.user.stats.gp + 10000,
-          'stats.mp': User.user.stats.mp + 10000
-        });
-      }
-      $scope.addOneLevel = function(){
-        User.set({
-          'stats.exp': User.user.stats.exp + (Math.round(((Math.pow(User.user.stats.lvl, 2) * 0.25) + (10 * User.user.stats.lvl) + 139.75) / 10) * 10)
-        });
-      }
-    }
+    // if (window.env.NODE_ENV === 'development') {
+    //   $scope.addMissedDay = function(){
+    //     if (!confirm("Are you sure you want to reset the day?")) return;
+    //     var dayBefore = moment(User.user.lastCron).subtract('days', 1).toDate();
+    //     User.set({'lastCron': dayBefore});
+    //     Notification.text('-1 day, remember to refresh');
+    //   }
+    //   $scope.addTenGems = function(){
+    //     $http.post(API_URL + '/api/v2/user/addTenGems').success(function(){
+    //       User.log({});
+    //     })
+    //   }
+    //   $scope.addLevelsAndGold = function(){
+    //     User.set({
+    //       'stats.exp': User.user.stats.exp + 10000,
+    //       'stats.gp': User.user.stats.gp + 10000,
+    //       'stats.mp': User.user.stats.mp + 10000
+    //     });
+    //   }
+    //   $scope.addOneLevel = function(){
+    //     User.set({
+    //       'stats.exp': User.user.stats.exp + (Math.round(((Math.pow(User.user.stats.lvl, 2) * 0.25) + (10 * User.user.stats.lvl) + 139.75) / 10) * 10)
+    //     });
+    //   }
+    // }
 
   }])
